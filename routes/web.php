6235e22a96b7b1ff44387fc66ec5aa0956bb5569
Lasantha Wijewardhana <lasanthakumara738@gmail.com>
2025-07-06 19:43:45 +0530

    Initial added

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+use PhpParser\Node\Expr\List_;
+use Termwind\Components\Li;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//All Listing
+Route::get('/', [ListingController::class, 'index']);
+
+//single Listing
+
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
