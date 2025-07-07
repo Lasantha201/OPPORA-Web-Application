561ba9e90f637a5187f542d2438165ddcba882d4
Lasantha Wijewardhana <lasanthakumara738@gmail.com>
2025-07-07 15:25:00 +0530

    New Initial added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -13,7 +13,7 @@ class ListingController extends Controller
         
         return view('listings.index', [
 
-        'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+        'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(4)
 ]);
     }
 
@@ -45,6 +45,10 @@ class ListingController extends Controller
         'tags' => 'required',
         'description' => 'required',
     ]);
+
+    if($request->hasFile('logo')){
+        $formFields['logo'] = $request->file('logo')->store('logos','public');
+    }
 
     Listing::create($formFields);
 
--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -2,7 +2,8 @@
 
 <x-card>
     <div class="flex">
-        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image1.png') }}" alt="" />
+        <img class="hidden w-48 mr-6 md:block"
+            src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image1.png') }}" alt="" />
 
         <div>
             <h3 class="text-2xl">
